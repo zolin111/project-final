0880c24fad3bd604cb14c60996224d5358f937a4
zolin111 <141412594+zolin111@users.noreply.github.com>
2023-08-13 20:17:59 +0300

    Add files via upload

---
+++ scripts/Установка openvpn-exporter.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+wget https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
+nano ~/.bashrc
+PATH=$PATH:$HOME/bin:/usr/local/go/bin
+source ~/.bashrc
+mkdir -p /opt/tools
+wget https://github.com/kumina/openvpn_exporter/archive/v0.3.0.tar.gz
+tar xzf v0.3.0.tar.gz
+sudo cd openvpn_exporter-0.3.0/
+sudo tee -a file << EOF main.og
+...
+func main() {
+        var (
+                listenAddress      = flag.String("web.listen-address", ":9176", "Address to listen on for web interface and telemetry.")
+                metricsPath        = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+                // openvpnStatusPaths = flag.String("openvpn.status_paths", "examples/client.status,examples/server2.status,examples/server3.status", "Paths at which OpenVPN places its status files.")
+                openvpnStatusPaths = flag.String("openvpn.status_paths", "/etc/openvpn/server/openvpn-status.log", "Paths at which OpenVPN places its status files.")
+EOF
+go build -o openvpn_exporter main.go
+sudo cp openvpn_exporter /usr/local/bin
+./openvpn_exporter
+sudo tee -a file <<EOF /etc/prometheus/prometheus.yml
+     ## Monitor OpenVPN
+  - job_name: 'openvpn-metrics'
+    scrape_interval: 5s
+    static_configs:
+            - targets: ['localhost:9176']
+EOF
+sudo systemctl restart prometheus
+sudo tee -a file <<EOF /etc/systemd/system/openvpn_exporter.service 
+[Unit]
+Description=Prometheus OpenVPN Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/openvpn_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl status openvpn_exporter
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scripts/Установка-openvpn-part2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+mkdir -p ~/clients/files
+chmod -R 700 ~/clients
+cd ~/easy-rsa
+openvpn --genkey --secret ta.key
+sudo cp ta.key /etc/openvpn/server/
+cd ~/easy-rsa
+./easyrsa gen-req client-1 nopass
+cp pki/private/client-1.key ~/clients/keys/
+cp ~/easy-rsa/ta.key ~/clients/keys/
+sudo cp /etc/openvpn/server/ca.crt ~/clients/keys/
+sudo cp /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz /etc/openvpn/server/
+sudo gunzip /etc/openvpn/server/server.conf.gz
+cd ~/clients
+cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ~/clients/base.conf
+ 
